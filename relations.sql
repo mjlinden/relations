8df9eb43ce518c7deb1f60f2402cede1f52dea5b
LINM04 <m.j.vd.linden@rocmondriaan.nl>
2022-11-30 08:42:37 +0100

    first commit

---
+++ relations.sql
@@ -0,0 +1,179 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 30 nov 2022 om 08:41
+-- Serverversie: 10.4.24-MariaDB
+-- PHP-versie: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `relations`
+--
+CREATE DATABASE IF NOT EXISTS `relations` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `relations`;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `category`
+--
+
+INSERT INTO `category` (`id`, `name`, `image`, `description`) VALUES
+(1, 'CPU', NULL, 'snelle cpu'),
+(2, 'keyboard', NULL, 'keyboard met verlichting'),
+(3, 'videocards', NULL, 'all videocards'),
+(4, 'motherboard', NULL, 'het hart van de computer'),
+(10, 'voeding', NULL, 'goede voedingen'),
+(13, 'mouses', NULL, NULL),
+(14, 'videos', NULL, 'dasdas');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `doctrine_migration_versions`
+--
+
+CREATE TABLE `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `doctrine_migration_versions`
+--
+
+INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+('DoctrineMigrations\\Version20221116083033', '2022-11-16 09:31:19', 106),
+('DoctrineMigrations\\Version20221116083806', '2022-11-16 09:38:11', 59),
+('DoctrineMigrations\\Version20221116085300', '2022-11-16 09:53:22', 130);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `messenger_messages`
+--
+
+CREATE TABLE `messenger_messages` (
+  `id` bigint(20) NOT NULL,
+  `body` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `headers` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue_name` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  `available_at` datetime NOT NULL,
+  `delivered_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `product`
+--
+
+INSERT INTO `product` (`id`, `category_id`, `name`, `image`, `description`) VALUES
+(1, 1, 'intel i7', 'intel.jpg', 'cpu van de 12e generatie'),
+(2, 2, 'trust keyboard', 'keyboard.jpg', NULL),
+(3, 1, 'amd r7', NULL, NULL);
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `doctrine_migration_versions`
+--
+ALTER TABLE `doctrine_migration_versions`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Indexen voor tabel `messenger_messages`
+--
+ALTER TABLE `messenger_messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_75EA56E0FB7336F0` (`queue_name`),
+  ADD KEY `IDX_75EA56E0E3BD61CE` (`available_at`),
+  ADD KEY `IDX_75EA56E016BA31DB` (`delivered_at`);
+
+--
+-- Indexen voor tabel `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_D34A04AD12469DE2` (`category_id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT voor een tabel `messenger_messages`
+--
+ALTER TABLE `messenger_messages`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT voor een tabel `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `FK_D34A04AD12469DE2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/Controller/BezoekerController.php
+++ src/Controller/BezoekerController.php
@@ -2,28 +2,77 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Entity\Product;
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BezoekerController extends AbstractController
 {
-    #[Route('/bezoeker', name: 'app_bezoeker')]
-    public function index(): Response
-    {
-        return $this->render('bezoeker/index.html.twig', [
-            'controller_name' => 'BezoekerController',
-        ]);
-    }
 
     #[Route('/products', name: 'app_products')]
     public function index2(ProductRepository $p): Response
     {
         $products=$p->findAll();
-        dd($products);
+
         return $this->render('bezoeker/index.html.twig', [
             'controller_name' => 'BezoekerController',
+            'products' => $products
         ]);
     }
+
+    #[Route('/categories', name: 'app_categories')]
+    public function index3(CategoryRepository $p): Response
+    {
+        $categories=$p->findAll();
+
+        return $this->render('bezoeker/categories.html.twig', [
+            'controller_name' => 'BezoekerController',
+            'categories' => $categories
+        ]);
+    }
+
+    #[Route('/insertcat', name: 'app_insert')]
+    public function index4(Request $request,ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $category=new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()) {
+            $category = $form->getData();
+            // tell Doctrine you want to (eventually) save the Product (no queries yet)
+            $entityManager->persist($category);
+
+            // actually executes the queries (i.e. the INSERT query)
+            $entityManager->flush();
+
+
+// add flash messages
+            $name=$category->getName();
+
+
+            $this->addFlash(
+                'notice',
+                "Rij  $name toegevoegd aan category tabel"
+            );
+
+
+            return $this->redirectToRoute('app_categories');
+        }
+        return $this->renderForm('bezoeker/new.html.twig', [
+            'form' => $form,
+        ]);
+
+    }
+
+
 }
